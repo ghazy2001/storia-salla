84620b1ff865f75ac0482afe2d66f7d1ea15b8f6
GhaZy <mahmoudghazy2001@gmail.com>
2026-02-21 15:28:16 +0200

    fix: justify-end flex-row-reverse to push size buttons to the right

--- src/components/product/ProductInfo.jsx
+++ src/components/product/ProductInfo.jsx
@@ -78,7 +78,7 @@ const ProductInfo = ({ product, handleAddToCart }) => {
                 </span>
               )}
             </p>
-            <div className="flex flex-wrap gap-3 justify-start flex-row-reverse">
+            <div className="flex flex-wrap gap-3 justify-end flex-row-reverse">
               {sizes.map((size) => (
                 <button
                   key={size}
